66a6dfa2036a04727f305ff03a306c3a6fcb817e
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-05-29 12:51:11 +0700

    membuat baseclass

---
+++ noOveridding/noOveridding.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class baseclass {
+public:
+	virtual void perkenalan() final {
+		cout << "Hallo saya Function dari base class";
+	}
+};
+
+
+int main() {
+
+}
